15a0e62112fdae013c305860d4cabbcc8b60afab
mcgraneder <mcgrane480@gmail.com>
2022-04-27 20:15:26 +0100

    fixed webpack polyfills issue changed to yarn over npm also

--- src/App.js
+++ src/App.js
@@ -1,25 +1,106 @@
-import logo from './logo.svg';
-import './App.css';
 
-function App() {
-  return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
+import { useMoralis, useEffct } from "react-moralis";
+import styles from "./Styles.css";
+import { useEffect, useState } from "react";
+import styled from "styled-components";
+
+export const Wrapper = styled.div`
+
+  height: 100vh;
+  width: 100%;
+  background: black;
+  display: flex;
+  align-items: center;
+  justofy-content: center;
+ 
+`
+
+export const FormWraper = styled.div`
+
+  margin: 0 auto;
+  background: White;
+  border-radius: 20px;
+`
+
+export const FormContainer = styled.div`
+
+  padding: 20px;
+`
+export default function App() {
+
+  const { authenticate, 
+    authError, 
+    isAuthenticating, 
+    isAuthenticated, 
+    account, 
+    logout 
+  } = useMoralis();
+
+ 
+
+  const [email, setEmail] = useState("");
+
+  useEffect(() => {
+    console.log(email)
+  }, [email])
+  
+  const handleCustomLogin = async () => {
+    await authenticate({
+      provider: "magicLink",
+      email: email,
+      apiKey: "pk_live_CEC575BEFD08F0C1",
+      chainId: "1",
+      network: "mainnet"
+    });
+  };
+
+  if(isAuthenticated) return (
+    <Wrapper className={styles.card}>
+    
+      <FormWraper>
+        <p style={{"textAlign": "center", "fontSize": "20px"}}>Logout</p>
+      {isAuthenticating && <p>Authenticating</p>}
+      {authError && (
+        <div>{JSON.stringify(authError.message)}</div>
+      )}
+       <FormContainer>
+        <button style={{"display": "block", "marginTop": "25px"}} onClick={() => logout()}>
+          Logout
+        </button>
+        {isAuthenticated && < div>{account}</div>}
+        </FormContainer>
+      </FormWraper>
+     
+    </Wrapper>
+
+  )
+
+  else return (
+    <Wrapper className={styles.card}>
+     
+      <FormWraper>
+
+        <p style={{"textAlign": "center", "fontSize": "20px"}}>Login with Fortmatic</p>
+      {isAuthenticating && <p>Authenticating</p>}
+      {authError && (
+        <div>{JSON.stringify(authError.message)}</div>
+      )}
+      <FormContainer style={{"display": "block"}}>
+        <input
+          type={"email"}
+          placeholder="Email"
+          value={email}
+          onChange={(e) => {
+            setEmail(e.target.value);
+          }}
+        />
+
+        <button style={{"display": "block", "marginTop": "25px"}} onClick={handleCustomLogin}>
+          Login with Magic Link
+        </button>
+        {isAuthenticated && < div>{account}</div>}
+        </FormContainer>
+      </FormWraper>
+    </Wrapper>
   );
 }
-
-export default App;
--- src/index.js
+++ src/index.js
@@ -3,11 +3,14 @@ import ReactDOM from 'react-dom/client';
 import './index.css';
 import App from './App';
 import reportWebVitals from './reportWebVitals';
+import { MoralisProvider } from 'react-moralis';
 
 const root = ReactDOM.createRoot(document.getElementById('root'));
 root.render(
   <React.StrictMode>
-    <App />
+    <MoralisProvider appId="y7HfxKGcU2syUHxzMZCuEPNTqjYeQXzbgMrRuudI" serverUrl="https://qxqezylenf64.usemoralis.com:2053/server">
+     <App />
+    </MoralisProvider>
   </React.StrictMode>
 );
 
